f17e003d2ce1414f79170aed33f24a2e1ff8a61c
rcebrian <rodrigocebriang@gmail.com>
2021-04-26 19:54:20 +0200

    :zap: roles middleware performance

--- src/middlewares/roles.middleware.ts
+++ src/middlewares/roles.middleware.ts
@@ -5,13 +5,13 @@ import { APIError } from '../errors';
 
 /**
  * Validate the input in requests
- * @param role
+ * @param roles possible roles
  * @param _req request with input form
  * @param _res response
  * @param _next next call
  * @returns
  */
-const checkRole = (role: string, _req: Request, _res: Response, _next: NextFunction) => {
+const checkRole = (roles: string[], _req: Request, _res: Response, _next: NextFunction) => {
   const bearer = _req.headers.authorization;
 
   if (!bearer) {
@@ -22,7 +22,7 @@ const checkRole = (role: string, _req: Request, _res: Response, _next: NextFunct
   const jwtSecret: any = process.env.JWT_SECRET;
 
   jwt.verify(token, jwtSecret, (err: any, payload: any) => {
-    if (!err && payload.role === role) {
+    if (!err && roles.indexOf(payload.role) > -1) {
       _next();
     } else {
       throw new APIError({ message: 'Unauthorized', stack: err, status: httpStatus.UNAUTHORIZED });
@@ -31,17 +31,17 @@ const checkRole = (role: string, _req: Request, _res: Response, _next: NextFunct
 };
 
 export const roleAdmin = (req: Request, res: Response, next: NextFunction) => {
-  checkRole('ROLE_ADMIN', req, res, next);
+  checkRole(['ROLE_ADMIN'], req, res, next);
 };
 
 export const roleCompany = (req: Request, res: Response, next: NextFunction) => {
-  checkRole('ROLE_COMPANY', req, res, next);
+  checkRole(['ROLE_COMPANY'], req, res, next);
 };
 
 export const roleMonitor = (req: Request, res: Response, next: NextFunction) => {
-  checkRole('ROLE_MONITOR', req, res, next);
+  checkRole(['ROLE_MONITOR'], req, res, next);
 };
 
 export const roleUser = (req: Request, res: Response, next: NextFunction) => {
-  checkRole('ROLE_USER', req, res, next);
+  checkRole(['ROLE_USER'], req, res, next);
 };

3a3ed82527ac0337ff588f50b9980c1ce8e13271
rcebrian <rodrigocebriang@gmail.com>
2021-05-08 01:37:57 +0200

    :sparkles: not-found handler

--- src/handlers/error.handler.ts
+++ src/handlers/error.handler.ts
@@ -2,23 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 
 import { APIError } from '../errors';
-
-const handler = (err: APIError, req: Request, res: Response, next: NextFunction) => {
-  const error = {
-    code: err.status || httpStatus.INTERNAL_SERVER_ERROR,
-    message: err.message || httpStatus[err.status],
-    errors: err.errors,
-    stack: err.stack,
-    timestamp: new Date().toISOString(),
-    URL: req.originalUrl,
-  };
-
-  if (process.env.APP_ENV !== 'development') {
-    delete error.stack;
-  }
-
-  res.status(error.code).json({ error });
-};
+import { handler } from './main.handler';
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   let convertedError = err;
